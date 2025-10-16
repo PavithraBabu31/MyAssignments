304464b232ea868866d5d35a0698a0964f703dde
PavithraBabu31 <pavithra.surendrababu31@gmail.com>
2025-10-16 09:49:52 +0530

    Add files via upload

---
+++ tests/Day1.js
@@ -0,0 +1,13 @@
+var FirstName='Pavithra'
+console.log("Firstname is :"+ FirstName)
+console.log(typeof firstName)
+var Companyname='HCL'
+console.log("Company is"+ Companyname)
+console.log(typeof Companyname)
+var MobileNo=9884347779
+console.log("My MobileNo is"+MobileNo)
+console.log(typeof MobileNo)
+var isAutomation=true
+console.log(typeof MobileNo)
+var hasPlaywright
+console.log(typeof hasPlaywright)
